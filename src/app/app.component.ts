01117ba48ea69cd510110ce90a6c5acc71dbc7a9
Modestas <mjovaisa@gmail.com>
2021-08-13 19:04:32 +0300

    Third

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './book';
+import { BookService } from './service/book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ElibraryAngular';
-}
+export class AppComponent implements OnInit {
+  public books?: Book[];
+
+  constructor(private bookService: BookService){}
+
+  ngOnInit(): void { }
+  }
+
